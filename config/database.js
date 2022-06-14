92fa11fa705319ee4bfe46028bdadb47d3ad9498
Hafizh Hasan Alauddin Rabbani <hafizhhafizh9@gmail.com>
2022-06-14 23:28:24 +0700

    dotenv,sequlize

--- config/database.js
+++ config/database.js
@@ -1,18 +1,15 @@
-/** Destruct environment variable to get database configuration */
-const {
-  DB_USERNAME = null,
-  DB_PASSWORD = null,
-  DB_HOST = "127.0.0.1",
-  DB_NAME = "database",
-} = process.env;
+require("dotenv").config();
+
+const { DB_USERNAME, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 module.exports = {
   development: {
     username: DB_USERNAME,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_development`,
+    database: `${DB_NAME}`,
     host: DB_HOST,
     dialect: "postgres",
+    ssl: true,
   },
   test: {
     username: DB_USERNAME,
@@ -20,6 +17,7 @@ module.exports = {
     database: `${DB_NAME}_test`,
     host: DB_HOST,
     dialect: "postgres",
+    ssl: true,
   },
   production: {
     username: DB_USERNAME,
@@ -27,5 +25,6 @@ module.exports = {
     database: `${DB_NAME}_production`,
     host: DB_HOST,
     dialect: "postgres",
+    ssl: true,
   },
 };
