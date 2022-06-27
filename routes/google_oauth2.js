9b5e5af580f728a5140dcda29e1303fa09d05a08
Hafizh Hasan Alauddin Rabbani <hafizhhafizh9@gmail.com>
2022-06-27 19:19:32 +0700

    Add Oauth with Passport

---
+++ routes/google_oauth2.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const passport = require("passport");
+const { router } = require("../server");
+const auth = require("../app/controllers/api/v1/auth/auth");
+const app = express();
+
+function isLoggedIn(req, res, next) {
+  req.user ? next() : res.sendStatus(401);
+}
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get("/login", (req, res) => {
+  res.send('<a href="/auth/google">Authenticate with Google</a>');
+});
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "/protected",
+    failureRedirect: "/auth/google/failure",
+  })
+);
+
+app.get("/protected", isLoggedIn, (req, res) => {
+  res.send(`Hello ${req.user.displayName}`);
+});
+
+app.get("/logout", (req, res) => {
+  req.logout();
+  req.session.destroy();
+  res.send("Goodbye!");
+});
+
+app.get("/auth/google/failure", (req, res) => {
+  res.send("Failed to authenticate..");
+});
+
+module.exports = router;
