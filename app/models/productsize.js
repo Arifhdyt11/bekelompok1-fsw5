d2f10728b5073bfc62b23dede2d90d86516c525b
iqbaliamal <iqbaliamal@gmail.com>
2022-07-01 02:58:54 +0700

    setup model, perubahan skema database

---
+++ app/models/productsize.js
@@ -0,0 +1,31 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class ProductSize extends Model {
+    static associate(models) {
+      this.hasOne(models.Transaction, { foreignKey: "productSizeId" });
+      this.belongsToMany(models.Product, {
+        through: "Product_Sizes",
+        as: "products",
+        foreignKey: "productId",
+      });
+      this.belongsToMany(models.Size, {
+        through: "Product_Sizes",
+        as: "sizes",
+        foreignKey: "sizeId",
+      });
+    }
+  }
+  ProductSize.init(
+    {
+      productId: DataTypes.STRING,
+      sizeId: DataTypes.STRING,
+      stock: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "ProductSize",
+    }
+  );
+  return ProductSize;
+};
