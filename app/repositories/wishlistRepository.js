e87e29a3d727ae9660a6295c53921907041ce9d9
Raihan Iqbal Pasya <raihaniqbalpasya.hsb@gmail.com>
2022-07-03 23:21:35 +0700

    fix response wishlist buyer

--- app/repositories/wishlistRepository.js
+++ app/repositories/wishlistRepository.js
@@ -18,6 +18,19 @@ module.exports = {
                 as: "categories",
                 attributes: ["name"],
               },
+              {
+                model: User,
+                as: "userAsSeller",
+                attributes: [
+                  "id",
+                  "role",
+                  "name",
+                  "city",
+                  "address",
+                  "phone",
+                  "image",
+                ],
+              }
             ],
           },
           {
@@ -27,6 +40,11 @@ module.exports = {
               role: "BUYER",
               id: id,
             },
+            attributes: [
+              "id",
+              "role",
+              "name",
+            ],
           },
         ],
       });
