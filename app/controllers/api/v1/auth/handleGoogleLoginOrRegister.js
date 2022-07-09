ea7679b019f8d4b7ce02eccd0bb950a0d049bf00
muhamadazis32 <muhamadazis9a@gmail.com>
2022-07-09 14:19:09 +0700

    fix Oauth token ga jalan

--- app/controllers/api/v1/auth/handleGoogleLoginOrRegister.js
+++ app/controllers/api/v1/auth/handleGoogleLoginOrRegister.js
@@ -1,43 +1,44 @@
 const axios = require("axios");
 const jwt = require("jsonwebtoken");
 const { User } = require("../../../../models");
-const { JWT_SECRET_KEY = "Rahasia" } = process.env;
+const { JWT_SECRET } = process.env;
 
 function createToken(user) {
   const payload = {
     id: user.id,
     name: user.name,
     email: user.email,
-    role: user.role,
   };
 
-  return jwt.sign(payload, JWT_SECRET_KEY);
+  return jwt.sign(payload, JWT_SECRET);
 }
 
 async function handleGoogleLoginOrRegister(req, res) {
-  const {access_token} = req.body;
+  const { access_token } = req.body;
 
   try {
-      const response = await axios.get(`https://www.googleapis.com/oauth2/v3/userinfo?access_token=${access_token}`);
-      const {sub, email, name} = response.data;
+    const response = await axios.get(
+      `https://www.googleapis.com/oauth2/v3/userinfo?access_token=${access_token}`
+    );
+    const { sub, email, name } = response.data;
 
-      let user = await User.findOne({where: {googleId: sub}});
-      if (!user)
-          user = await User.create({
-              email,
-              name,
-              googleId: sub,
-              role: "SELLER",
-          });
+    let user = await User.findOne({ where: { googleId: sub } });
+    if (!user)
+      user = await User.create({
+        email,
+        name,
+        googleId: sub,
+        role: "BUYER",
+      });
+    delete user.password;
 
-      delete user.password;
+    const token = createToken(user);
+    res.status(201).json({token});
 
-      const token = createToken(user);
 
-      res.status(201).json({token});
   } catch (err) {
-      console.log(err.message);
-      res.status(401).json({error: {name: err.name, message: err.message}});
+    console.log(err.message);
+    res.status(401).json({ error: { name: err.name, message: err.message } });
   }
 }
 
