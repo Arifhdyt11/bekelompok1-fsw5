da99e5ccfb04ec3e65e192edc1a16813ba226d69
iqbaliamal <iqbaliamal@gmail.com>
2022-07-24 19:16:30 +0700

    refactoring module product

--- app/controllers/api/v1/productController.js
+++ app/controllers/api/v1/productController.js
@@ -1,8 +1,4 @@
 const productService = require("../../../services/productService");
-const { promisify } = require("util");
-const cloudinary = require("../../../../config/cloudinary");
-const cloudinaryUpload = promisify(cloudinary.uploader.upload);
-const cloudinaryDestroy = promisify(cloudinary.uploader.destroy);
 
 const socket = require("../../../../bin/www"); //import socket  from www
 
@@ -10,7 +6,6 @@ module.exports = {
   async list(req, res) {
     try {
       const data = await productService.list();
-
       socket.ioObject.emit("products", data);
       res.status(200).json({
         status: true,
@@ -41,13 +36,48 @@ module.exports = {
     }
   },
 
+  async show(req, res) {
+    try {
+      const data = await productService.get(req.params.id);
+      res.status(200).json({
+        status: true,
+        message: "Successfully find data",
+        data: data,
+      });
+    } catch (error) {
+      res.status(422).json({
+        status: false,
+        message: error.message,
+      });
+    }
+  },
+
+  async showBySeller(req, res) {
+    try {
+      const data = await productService.getBySellerId(
+        req.params.id,
+        req.user.id
+      );
+      res.status(200).json({
+        status: true,
+        message: "Successfully find data",
+        data: data,
+      });
+    } catch (error) {
+      res.status(422).json({
+        status: false,
+        message: error.message,
+      });
+    }
+  },
+
   async create(req, res) {
     try {
       const requestBody = req.body;
       const sellerId = req.user.id;
       const requestFile = req.files;
 
-      const data = await productService.handleCreateProduct(
+      const data = await productService.create(
         requestBody,
         sellerId,
         requestFile
@@ -66,110 +96,23 @@ module.exports = {
     }
   },
 
-  async show(req, res) {
-    try {
-      const data = await productService.get(req.params.id);
-      if (data !== null) {
-        res.status(200).json({
-          status: true,
-          message: "Successfully find data",
-          data: data,
-        });
-      } else {
-        res.status(404).json({
-          status: false,
-          message: "Data not found",
-        });
-      }
-    } catch (error) {
-      res.status(422).json({
-        status: false,
-        message: error.message,
-      });
-    }
-  },
-
-  async showBySeller(req, res) {
-    try {
-      const data = await productService.getBySellerId(
-        req.params.id,
-        req.user.id
-      );
-      if (data !== null) {
-        res.status(200).json({
-          status: true,
-          message: "Successfully find data",
-          data: data,
-        });
-      } else {
-        res.status(404).json({
-          status: false,
-          message: "Data not found",
-        });
-      }
-    } catch (error) {
-      res.status(422).json({
-        status: false,
-        message: error.message,
-      });
-    }
-  },
-
   async update(req, res) {
     try {
-      const oldImage = req.body.oldImage;
-      const fileBase64 = [];
-      const file = [];
-      const newImage = [];
-      // console.log("old image : ", oldImage.length);
-      let updateArgs = {
-        ...req.body,
-      };
-      let productId = req.params.id;
+      const productId = dataProductById.id;
+      const sellerId = req.user.id;
+      const requestBody = req.body;
+      const requestFile = req.files;
 
-      const dataUpdated = await productService.get(productId);
-
-      // Delete Image from Cloudinary
-      if (oldImage) {
-        if (Array.isArray(oldImage)) {
-          // Kalo bentuknya array
-          for (var x = 0; x < oldImage.length; x++) {
-            cloudinaryDestroy(oldImage[x]);
-          }
-        } else {
-          // Kalo bentuknya string cuma 1 image
-          cloudinaryDestroy(oldImage);
-        }
-      }
-      // Upload New Image to Cloudinary
-      const image = req.body.image;
-      if (image) {
-        if (Array.isArray(image)) {
-          for (var x = 0; x < image.length; x++) {
-            newImage.push(image[x]);
-          }
-        } else {
-          newImage.push(image);
-        }
-      }
-      if (req.files) {
-        if (req.files.length > 0) {
-          for (var i = 0; i < req.files.length; i++) {
-            fileBase64.push(req.files[i].buffer.toString("base64"));
-            file.push(`data:${req.files[i].mimetype};base64,${fileBase64[i]}`);
-            const result = await cloudinaryUpload(file[i]);
-            newImage.push(result.secure_url);
-          }
-        }
-      }
-      updateArgs = { ...updateArgs, image: newImage };
-      console.log(updateArgs);
-      await productService.update(productId, updateArgs);
-      socket.ioObject.emit("update-products");
+      const data = await productService.update(
+        productId,
+        sellerId,
+        requestBody,
+        requestFile
+      );
       res.status(200).json({
         status: true,
         message: "Product has been updated!",
-        data: dataUpdated,
+        data: data,
       });
     } catch (err) {
       res.status(422).json({
@@ -179,16 +122,9 @@ module.exports = {
     }
   },
 
-  // deleteProduct : async (req,res, next) => {
-  //   try{
-  //     const productRepository = await
-  //   }
-  // }
-
   async destroy(req, res) {
     try {
       await productService.delete(req.params.id);
-      socket.ioObject.emit("delete-products");
       res.status(200).json({
         status: true,
         message: "Product has been deleted!",
--- app/repositories/productRepository.js
+++ app/repositories/productRepository.js
@@ -1,9 +1,9 @@
-const { Product, Category, User, Size } = require("../models");
+const { Product, Category, User } = require("../models");
 
 module.exports = {
-  findAll() {
+  async findAll() {
     try {
-      const data = Product.findAll({
+      return await Product.findAll({
         include: [
           {
             model: Category,
@@ -21,36 +21,26 @@ module.exports = {
           "updatedAt",
         ],
       });
-
-      if (data) {
-        return data;
-      }
-      console.log(data);
     } catch (error) {
       return error;
     }
   },
 
-  findByIdCreate(id) {
+  async findByIdCreate(id) {
     try {
-      const data = Product.findOne({
+      return await Product.findOne({
         where: {
           id: id,
         },
       });
-
-      if (data) {
-        return data;
-      }
-      console.log(data);
     } catch (error) {
       return error;
     }
   },
 
-  findById(id) {
+  async findById(id) {
     try {
-      const data = Product.findOne({
+      return await Product.findOne({
         include: [
           {
             model: Category,
@@ -76,18 +66,14 @@ module.exports = {
         },
         attributes: ["id", "name", "image", "price", "description", "status"],
       });
-
-      if (data) {
-        return data;
-      }
     } catch (error) {
       return error;
     }
   },
 
-  findBySellerId(id, sellerId) {
+  async findBySellerId(id, sellerId) {
     try {
-      return Product.findOne({
+      return await Product.findOne({
         where: {
           id: id,
           userId: sellerId,
@@ -127,9 +113,9 @@ module.exports = {
     }
   },
 
-  findBySeller(sellerId) {
+  async findBySeller(sellerId) {
     try {
-      return Product.findAll({
+      return await Product.findAll({
         where: {
           userId: sellerId,
         },
@@ -147,61 +133,20 @@ module.exports = {
     }
   },
 
-  find(id) {
-    try {
-      const data = Product.findOne({
-        include: [
-          {
-            model: Category,
-            as: "categories",
-            attributes: ["name"],
-          },
-          {
-            model: User,
-            as: "userAsSeller",
-            attributes: [
-              "role",
-              "name",
-              "email",
-              "city",
-              "address",
-              "phone",
-              "status",
-            ],
-          },
-          {
-            model: Size,
-            as: "sizes",
-            attributes: ["size", "stock"],
-          },
-        ],
-        where: {
-          id: id,
-        },
-      });
-
-      if (data) {
-        return data;
-      }
-    } catch (error) {
-      return error;
-    }
+  async create(createArgs) {
+    return await Product.create(createArgs);
   },
 
-  create(createArgs) {
-    return Product.create(createArgs);
-  },
-
-  update(id, updateArgs) {
-    return Product.update(updateArgs, {
+  async update(id, updateArgs) {
+    return await Product.update(updateArgs, {
       where: {
         id,
       },
     });
   },
 
-  delete(id) {
-    return Product.destroy({
+  async delete(id) {
+    return await Product.destroy({
       where: {
         id,
       },
